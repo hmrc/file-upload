25d813ff939f9314c535758b21631a1d6b9e6e9a
Josiah Ebhomenye <josia.ebhomenye@news.co.uk>
2016-06-22 11:12:03 +0100

    added more support code

--- it/uk/gov/hmrc/fileupload/controllers/FileUploadIntegrationSpec.scala
+++ it/uk/gov/hmrc/fileupload/controllers/FileUploadIntegrationSpec.scala
@@ -1,11 +1,29 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.fileupload.controllers
 
+import java.util.UUID
 
-import play.api.test.PlaySpecification
 import play.api.libs.ws._
-import play.api.test._
+import play.api.test.PlaySpecification
 
 class FileUploadIntegrationSpec extends PlaySpecification{
+
+	val nextId = () => UUID.randomUUID().toString
 
   val poem =     """
                    |Do strangers make you human
@@ -25,17 +43,19 @@ class FileUploadIntegrationSpec extends PlaySpecification{
 
   "Application" should{
     "be able to process an upload request" in  {
-
+			val id = nextId()
       val response: WSResponse = await(
         support
           .withEnvelope
-          .flatMap(_.doUpload(data, filename = "poem.txt"))
+          .flatMap(_.doUpload(data, fileId = id))
       )
       val filename = await(support.refresh.map(_.mayBeEnvelope.get.files.head.head))
+			val storedPoem = await(support.getFile(id).map(new String(_)))
 
       response.status mustEqual OK
       filename mustEqual "poem.txt"
-      // TODO check file contents are the same
+	    storedPoem mustEqual poem
+
     }
   }
 
--- it/uk/gov/hmrc/fileupload/controllers/FileUploadSupport.scala
+++ it/uk/gov/hmrc/fileupload/controllers/FileUploadSupport.scala
@@ -1,15 +1,37 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.fileupload.controllers
 
+import java.io.File
 import java.util.UUID
 
 import org.joda.time.DateTime
-import play.api.libs.json.{JsValue, JsString, Json}
+import play.api.libs.iteratee.Iteratee
+import play.api.libs.json.{JsString, JsValue, Json}
+import play.api.libs.ws._
 import play.api.test.WithServer
+import reactivemongo.api.gridfs.GridFS
+import reactivemongo.json.JSONSerializationPack
+import uk.gov.hmrc.fileupload._
 import uk.gov.hmrc.fileupload.models.{Constraints, Envelope}
 import uk.gov.hmrc.fileupload.repositories.{DefaultMongoConnection, EnvelopeRepository}
 
 import scala.concurrent.{ExecutionContext, Future}
-import play.api.libs.ws._
+import scala.io.Source
 
 /**
   * Created by Josiah on 6/20/2016.
@@ -27,6 +49,10 @@ class FileUploadSupport(var mayBeEnvelope: Option[Envelope] = None) extends With
     constraints = Constraints(contentTypes = Seq("application/vnd.openxmlformats-officedocument.wordprocessingml.document"), maxItems = 100, maxSize = "12GB", maxSizePerItem = "10MB")
     , callbackUrl = "http://absolute.callback.url", expiryDate = DateTime.now().plusDays(1), metadata = Map("anything" -> JsString("the caller wants to add to the envelope")))
   )).getBytes
+
+	def createEnnvelope(file: File) = createEnvelope(Source.fromFile(file).mkString)
+
+	def createEnvelope(data: String): Future[WSResponse] = createEnvelope(data.getBytes())
 
 	def createEnvelope(data: Array[Byte]): Future[WSResponse] = {
 		WS
@@ -64,13 +90,26 @@ class FileUploadSupport(var mayBeEnvelope: Option[Envelope] = None) extends With
 	    }
   }
 
-  def doUpload(data: Array[Byte], filename: String = "test.data"): Future[WSResponse] = {
+  def doUpload(data: Array[Byte], fileId: String): Future[WSResponse] = {
     require(mayBeEnvelope.isDefined, "No envelope defined")
     WS
-      .url(s"$url/${mayBeEnvelope.get._id}/file/$filename/content")
+      .url(s"$url/${mayBeEnvelope.get._id}/file/$fileId/content")
       .withHeaders("Content-Type" -> "application/octet-stream")
       .put(data)
   }
+
+	def getFile(id: String): Future[ByteStream] = {
+		import reactivemongo.json.collection._
+		val gfs: JSONGridFS = GridFS[JSONSerializationPack.type](DefaultMongoConnection.db() , "envelopes")
+		gfs.find[JsValue, JSONReadFile](Json.obj("_id" -> id)).headOption.flatMap{ mayBeReadFile =>
+			mayBeReadFile.map{ readFile =>
+				val enumerator = gfs.enumerate(readFile)
+				val consumer = Iteratee.consume[ByteStream]()
+				enumerator |>>> consumer
+			}.get
+		}
+
+	}
 
 
 }
