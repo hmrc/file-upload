f1cc2df2dfb7ac9e3949b7ff0bc44b016b1d427b
Craig Duncan <craig@CraigMBPR.local>
2016-07-05 16:51:08 +0100

    FILE-63: Updated acceptance tests based on latest criteria

--- it/uk/gov/hmrc/fileupload/EnvelopeIntegrationSpec.scala
+++ it/uk/gov/hmrc/fileupload/EnvelopeIntegrationSpec.scala
@@ -5,23 +5,27 @@ import org.joda.time.format.DateTimeFormat
 import play.api.libs.json._
 import play.api.libs.ws._
 import play.api.test._
+import uk.gov.hmrc.fileupload.models._
+import org.scalatest._
 
-
+/**
+  * Integration tests for FILE-63 & FILE-64
+  * Create Envelope and Get Envelope
+  *
+  * Currently configured to run against a local instance of the File Upload service
+  * Needs to be converted to FakeApplication tests and the contract level acceptance tests re-written
+  *
+  */
 class EnvelopeIntegrationSpec extends PlaySpecification {
 
-  val serverUrl = "http://localhost:9000"
-
+  val ServerUrl = "http://localhost:9000"
+  val CreateEnvelopeResourcePath = "/file-upload/envelope"
+  val GetEnvelopeResourcePath = "/file-upload/envelope/:envelopeId"
   val formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
   val today = new DateTime().plusMinutes(10)
 
-  var envelopeId : String = null
-
-
   "Create Envelope request with basic sample" should {
-
-    "complete successfully with a CREATED response and provide the Envelope ID within the Location header" in new WithServer() {
-
-      val createEnvelopeResourcePath = "/file-upload/envelope"
+    "complete successfully with a 201 Created response and provide the Envelope ID within the Location header" in new WithServer() {
       val formattedExpiryDate: String = formatter.print(today)
       val json = Json.parse(
         s"""
@@ -42,39 +46,81 @@ class EnvelopeIntegrationSpec extends PlaySpecification {
            |  }
            |}
         """.stripMargin)
-
-      val finalUrl = serverUrl + createEnvelopeResourcePath
-
+      val finalUrl = ServerUrl + CreateEnvelopeResourcePath
       val response = await(WS.url(finalUrl).post(json))
       response.status must equalTo(CREATED)
-
       val locationHeader = response.header("Location").get
-      envelopeId = locationHeader.substring(locationHeader.lastIndexOf('/')+1)
-      println("envelopeId: " + envelopeId)
+      val envelopeId = locationHeader.substring(locationHeader.lastIndexOf('/') + 1)
+      envelopeId mustNotEqual null
+      println(s"envelopeId: $envelopeId")
     }
   }
 
+  "Create Envelope request with {} body" should {
+    "completed successfully with a 201 Created response and provide the Envelope ID within the Location header" in new WithServer() {
+      val json = Json.parse("{}")
+      val finalUrl = ServerUrl + CreateEnvelopeResourcePath
+      val response = await(WS.url(finalUrl).post(json))
+      response.status must equalTo(CREATED)
+      val locationHeader = response.header("Location").get
+      val envelopeId = locationHeader.substring(locationHeader.lastIndexOf('/') + 1)
+      println(s"envelopeId: $envelopeId")
+    }
+  }
+
+  "Create Envelope request with null body" should {
+    "fail with a 400 Bad Request error" in new WithServer() {
+      val finalUrl = ServerUrl + CreateEnvelopeResourcePath
+      val response = await(WS.url(finalUrl).post(null))
+      response.status must equalTo(BAD_REQUEST)
+    }
+  }
+
+  "Create Envelope request which contains an Envelope ID" should {
+    "fail with a 400 Bad Request error" in new WithServer() {
+      val formattedExpiryDate: String = formatter.print(today)
+      val json = Json.parse(
+        s"""
+           |{"_id": "92e8fcdd-2911-464f-8b52-8fb1f05206fb",
+           |"constraints": {
+           |    "contentTypes": [
+           |      "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+           |      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+           |      "application/vnd.oasis.opendocument.spreadsheet"
+           |    ],
+           |    "maxItems": 100,
+           |    "maxSize": "12GB",
+           |    "maxSizePerItem": "10MB"
+           |  },
+           |  "callbackUrl": "http://absolute.callback.url",
+           |  "expiryDate": "$formattedExpiryDate",
+           |  "metadata": {
+           |    "anything": "the caller wants to add to the envelope"
+           |  }
+           |}
+        """.stripMargin)
+
+      val finalUrl = ServerUrl + CreateEnvelopeResourcePath
+      val response = await(WS.url(finalUrl).post(json))
+      response.status must equalTo(BAD_REQUEST)
+    }
+  }
 
   "GET Envelope request with a valid Envelope ID" should {
-
     "complete successfully with an OK response and provide the envelope details in the body" in new WithServer() {
 
-      println("getting envelope for : " + envelopeId)
+      // Create an envelope to retrieve an envelope ID
+      val finalCreateUrl = ServerUrl + CreateEnvelopeResourcePath
+      val createResponse = await(WS.url(finalCreateUrl).post(Json.parse("{}")))
+      createResponse.status must equalTo(CREATED)
+      val locationHeader = createResponse.header("Location").get
+      val envelopeId = locationHeader.substring(locationHeader.lastIndexOf('/') + 1)
 
-      val resourcePath = "/file-upload/envelope/{envelopeId}"
-
-      val finalUrl = serverUrl + resourcePath.replace("{envelopeId}", envelopeId)
-
-      val response = await(WS.url(finalUrl).get())
-      response.status must equalTo(OK)
-      println("***************************")
-      println(response.body)
-      println("***************************")
-
-      response.body mustNotEqual(null)
-
-//      val envelope = Json.fromJson[Envelope](response.json).get
-//      envelope._id mustEqual envelopeId
+      // Retrieve envelope ID
+      val finalGetUrl = ServerUrl + GetEnvelopeResourcePath.replace(":envelopeId",envelopeId)
+      val getResponse = await(WS.url(finalGetUrl).get())
+      getResponse.status must equalTo(OK)
+      getResponse.body mustNotEqual(null)
     }
   }
 
