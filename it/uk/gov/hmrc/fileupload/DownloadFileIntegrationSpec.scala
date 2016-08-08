52a2f09d947e5a3b1023a5dcffb9478e1ce62259
Craig Duncan <craig@CraigMBPR.local>
2016-08-08 16:33:26 +0100

    Integration test aligned with acceptance criteria and now validates file name and checksum of downloaded file against spec/original

--- it/uk/gov/hmrc/fileupload/DownloadFileIntegrationSpec.scala
+++ it/uk/gov/hmrc/fileupload/DownloadFileIntegrationSpec.scala
@@ -1,7 +1,11 @@
 package uk.gov.hmrc.fileupload
 
+import java.io.RandomAccessFile
+
 import play.api.libs.ws._
+import uk.gov.hmrc.fileupload.support.FileMetadataReportSupport._
 import uk.gov.hmrc.fileupload.support.{EnvelopeActions, FileActions, IntegrationSpec}
+
 
 /**
   * Integration tests for FILE-104
@@ -24,7 +28,7 @@ class DownloadFileIntegrationSpec extends IntegrationSpec with FileActions with 
       When(s"I invoke GET envelope/$envelopeId/file/$fileId/content")
       val response: WSResponse = download(envelopeId, fileId)
 
-      Then("I will receive a 200 Created response")
+      Then("I will receive a 200 OK response")
       response.status shouldBe OK
 
       And("I will receive the file")
@@ -49,6 +53,48 @@ class DownloadFileIntegrationSpec extends IntegrationSpec with FileActions with 
 
       Then("I will receive a 404 Not Found response")
       response.status shouldBe NOT_FOUND
+    }
+    
+    scenario("Valid file can be downloaded") {
+      Given("I have a valid envelope ID")
+      val envelopeId = createEnvelope()
+
+      And("I have a valid file ID")
+      val fileId = FileId(s"fileId-${nextId()}")
+
+      And("a file name has been provided within the file metadata")
+      val newFileName = "new-file-name.pdf"
+      val json = requestBody(Map("name" -> newFileName))
+      updateFileMetadata(json, envelopeId, fileId)
+
+      And("a file has previously been uploaded to the transient store")
+      val file = new RandomAccessFile("t", "rw")
+      file.setLength(1024 * 1024 * 2)
+      val data = new Array[Byte](file.length().toInt)
+      file.readFully(data)
+      val putFileResponse: WSResponse = upload(data, envelopeId, fileId)
+      val md = java.security.MessageDigest.getInstance("SHA-256")
+      md.reset()
+      md.update(data)
+      val sourceDigest = md.digest()
+
+      When("I call GET /file-upload/envelope/:envelope-id/file/:file-id/content")
+      val getFileResponse : WSResponse = download(envelopeId, fileId)
+
+      Then("I will receive a 200 OK response")
+      getFileResponse.status shouldBe OK
+
+      And("the file is downloaded successfully")
+      val storedFile : Array[Byte] = getFileResponse.body.getBytes
+      md.reset()
+      md.update(storedFile)
+      val storedDigest = md.digest()
+
+      And("the filename within the metadata has been applied")
+      getFileResponse.header("Content-Disposition") shouldBe Some(s"""attachment; filename=\"$newFileName"""")
+
+      And("the downloaded file is identical to the original file")
+      sourceDigest shouldEqual storedDigest
     }
   }
 }
