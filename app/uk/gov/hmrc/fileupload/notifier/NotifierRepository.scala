225561563fa8da3329995182af68b10b681b048e
Hannes Stockner <hannes.stockner@digital.hmrc.gov.uk>
2016-08-24 15:07:35 +0100

    Send json when calling callback url.

--- app/uk/gov/hmrc/fileupload/notifier/NotifierRepository.scala
+++ app/uk/gov/hmrc/fileupload/notifier/NotifierRepository.scala
@@ -19,6 +19,7 @@ package uk.gov.hmrc.fileupload.notifier
 import cats.data.Xor
 import play.api.Play.current
 import play.api.http.Status
+import play.api.libs.json.{Format, Json}
 import play.api.libs.ws.{WS, WSRequestHolder, WSResponse}
 import uk.gov.hmrc.fileupload.infrastructure.PlayHttp.PlayHttpError
 import uk.gov.hmrc.fileupload.{EnvelopeId, FileId}
@@ -31,6 +32,8 @@ object NotifierRepository {
 
   case class Notification(envelopeId: EnvelopeId, fileId: FileId, status: String, reason: Option[String])
 
+  implicit val notificationFormats: Format[Notification] = Json.format[Notification]
+
   sealed trait NotifyError
   case class NoConsumerRegisteredError(envelopeId: EnvelopeId, fileId: FileId) extends NotifyError
   case class NotificationFailedError(envelopeId: EnvelopeId, fileId: FileId, reason: String) extends NotifyError
@@ -38,9 +41,10 @@ object NotifierRepository {
   def notify(httpCall: (WSRequestHolder => Future[Xor[PlayHttpError, WSResponse]]))
             (notification: Notification, url: String)
             (implicit executionContext: ExecutionContext): Future[NotifyResult] =
-    httpCall(WS.url(
-      s"$url?envelopeId=${notification.envelopeId.value}&fileId=${notification.fileId.value}&reason=${notification.reason}")
-      .withMethod("GET")).map {
+    httpCall(WS
+      .url(s"$url")
+      .withBody(Json.stringify(Json.toJson(notification)))
+      .withMethod("POST")).map {
       case Xor.Left(error) => Xor.left(NotificationFailedError(notification.envelopeId, notification.fileId, error.message))
       case Xor.Right(response) => response.status match {
         case Status.OK => Xor.right(notification.envelopeId)
