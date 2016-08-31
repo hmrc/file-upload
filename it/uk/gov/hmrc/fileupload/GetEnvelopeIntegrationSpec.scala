c7356c3c5e540574dc11654794bd03bdf1415e02
Antonio Antonelli <me@antonioantonelli.com>
2016-08-31 10:16:19 +0100

    more tests for get envelope

--- it/uk/gov/hmrc/fileupload/GetEnvelopeIntegrationSpec.scala
+++ it/uk/gov/hmrc/fileupload/GetEnvelopeIntegrationSpec.scala
@@ -1,14 +1,14 @@
 package uk.gov.hmrc.fileupload
 
 import play.api.libs.json._
-import uk.gov.hmrc.fileupload.support.{EnvelopeActions, IntegrationSpec}
+import uk.gov.hmrc.fileupload.support.{EnvelopeActions, FileActions, IntegrationSpec}
 
 /**
   * Integration tests for FILE-63 & FILE-64
   * Create Envelope and Get Envelope
   *
   */
-class GetEnvelopeIntegrationSpec extends IntegrationSpec with EnvelopeActions {
+class GetEnvelopeIntegrationSpec extends IntegrationSpec with EnvelopeActions with FileActions {
 
   feature("Retrieve Envelope") {
 
@@ -36,6 +36,43 @@ class GetEnvelopeIntegrationSpec extends IntegrationSpec with EnvelopeActions {
 
       parsedBody \ "status" match {
         case JsString(value) =>  value should fullyMatch regex "OPEN"
+        case _ => JsError("expectation failed")
+      }
+
+    }
+
+    scenario("GET Envelope responds with a list of files when envelope not empty") {
+      Given("I have an envelope with files")
+      val createResponse = createEnvelope("{}")
+      createResponse.status should equal(CREATED)
+      val envelopeId = envelopeIdFromHeader(createResponse)
+      val fileId = FileId("myfileid")
+      upload("my file content".getBytes(), envelopeId, fileId)
+
+      When("I call GET /file-upload/envelope/:envelope-id")
+      val envelopeResponse = getEnvelopeFor(envelopeId)
+
+      Then("I will receive a 200 Ok response")
+      envelopeResponse.status shouldBe OK
+
+      And("the response body should contain the envelope details")
+      val body: String = envelopeResponse.body
+      body shouldNot be(null)
+
+      val parsedBody: JsValue = Json.parse(body)
+      parsedBody \ "id" match {
+        case JsString(value) =>  value should fullyMatch regex "[A-z0-9-]+"
+        case _ => JsError("expectation failed")
+      }
+
+      parsedBody \ "status" match {
+        case JsString(value) =>  value should fullyMatch regex "OPEN"
+        case _ => JsError("expectation failed")
+      }
+
+      (parsedBody \ "files").asInstanceOf[JsArray].value.head \ "id" match {
+        case JsObject(value) =>
+          value shouldBe fileId
         case _ => JsError("expectation failed")
       }
 
