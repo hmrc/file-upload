e7ff348ecc8994b723817950b28e73a61baeb75f
paul.tan <paul.tan@digital.hmrc.gov.uk>
2016-09-28 12:02:54 +0100

    change 202 to 200 for delete envelope integration test

--- it/uk/gov/hmrc/fileupload/DeleteEnvelopeIntegrationSpec.scala
+++ it/uk/gov/hmrc/fileupload/DeleteEnvelopeIntegrationSpec.scala
@@ -21,8 +21,8 @@ class DeleteEnvelopeIntegrationSpec extends IntegrationSpec with Eventually with
       When("I call DELETE /file-upload/envelopes/:envelope-id")
       val envelopeResponse = deleteEnvelopFor(envelopeId)
 
-      Then("I will receive a 202 Accpeted response")
-      envelopeResponse.status shouldBe ACCEPTED
+      Then("I will receive a 200 OK response")
+      envelopeResponse.status shouldBe OK
 
       eventually {
         And("the envelope should be deleted")
