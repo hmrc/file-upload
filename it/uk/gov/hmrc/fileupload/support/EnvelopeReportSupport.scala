1abc2296ac65fde2e0eb05610a3571f56212c3a1
Craig Duncan <craig@CraigMBPR.local>
2017-05-11 12:22:12 +0100

    Added integration support to create an envelope with constraints on contentType, maxSize, maxSizePerItem but NOT maxItems

--- it/uk/gov/hmrc/fileupload/support/EnvelopeReportSupport.scala
+++ it/uk/gov/hmrc/fileupload/support/EnvelopeReportSupport.scala
@@ -16,4 +16,23 @@ object EnvelopeReportSupport extends Support {
      |}
 		 """.stripMargin
 
+
+
+  def requestBodyWithConstraints(args: Map[String, Any] = Map.empty) = s"""
+       |{
+       |  "constraints" : {
+       |    "contentTypes" : [
+       |      "${ args.getOrElse("contentType", "application/xml") }"
+       |    ],
+       |    "maxSize" : "${ args.getOrElse("maxSize", "12MB") }",
+       |    "maxSizePerItem" : "${ args.getOrElse("maxSizePerItem", "10MB") }"
+       |  },
+       |  "callbackUrl" : "${args.getOrElse("callbackUrl", "http://localhost:8900")}",
+       |  "expiryDate" : "${args.getOrElse("formattedExpiryDate", "2099-07-14T10:28:18Z")}",
+       |  "metadata" : {
+       |    "anything" : "the caller wants to add to the envelope"
+       |  }
+       |}
+		 """.stripMargin
+
 }
