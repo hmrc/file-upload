9e1d02573a4362a90dbe86bb5994e7e3410e0a99
Tomasz Rosiek <tomasz.rosiek@digital.hmrc.gov.uk>
2018-05-25 14:23:41 +0100

    Fixed LDS warnings

--- it/uk/gov/hmrc/fileupload/support/FileMetadataReportSupport.scala
+++ it/uk/gov/hmrc/fileupload/support/FileMetadataReportSupport.scala
@@ -19,7 +19,7 @@ object FileMetadataReportSupport extends Support {
      |      "session":"${args.getOrElse("metadata.origin.session", "cd30f8ec-d866-4ae0-82a0-1bc720f1cb09")}",
      |      "agent":"${args.getOrElse("metadata.origin.agent", "292929292")}",
      |      "trustedHelper":"${args.getOrElse("metadata.origin.trustedHelper", "8984293480239480")}",
-     |      "ipAddress":"${args.getOrElse("metadata.origin.ipAddress", "127.0.0.1")}"
+     |      "ipAddress":"${args.getOrElse("metadata.origin.ipAddress", "10.0.0.1")}"
      |    },
      |    "sender":{
      |      "service":"${args.getOrElse("metadata.sender.service", "some-service-identifier/v1.2.33")}"
