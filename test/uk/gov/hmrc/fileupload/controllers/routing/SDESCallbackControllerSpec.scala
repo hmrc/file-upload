a08e5172fdf2246d893fd1173c474859f17b05c5
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2022-07-25 13:24:39 +0100

    BDOG-1987 Mark envelope as closed only when receive 'FileReceived' notification - it will be retried periodically until we receive this

--- test/uk/gov/hmrc/fileupload/controllers/routing/SDESCallbackControllerSpec.scala
+++ test/uk/gov/hmrc/fileupload/controllers/routing/SDESCallbackControllerSpec.scala
@@ -53,9 +53,9 @@ class SDESCallbackControllerSpec
 
   "callback" should {
 
-    val justReturn200s = Seq(FileReceived, FileReady, FileProcessingFailure)
+    val notificationTypesJustReturn200s = Seq(FileReady, FileProcessingFailure)
 
-    justReturn200s.foreach { notification =>
+    notificationTypesJustReturn200s.foreach { notification =>
       s"return response with 200 if notification is ${notification.value}" in {
         val request = FakeRequest().withBody(Json.toJson(notificationItem(notification)))
 
@@ -66,25 +66,38 @@ class SDESCallbackControllerSpec
       }
     }
 
-    "return response with 500 if handler returns error for FileProcessed" in {
-      val request = FakeRequest().withBody(Json.toJson(notificationItem(FileProcessed)))
+    val notificationTypesToBeProcessed = Seq(FileReceived, FileProcessed)
 
-      val controller = newController(handleCommand = _ => Future.successful(Left(CommandError("not good"))))
-      val result = controller.callback()(request).futureValue
+    notificationTypesToBeProcessed.foreach { notification =>
+      s"return response with 500 if handler returns error for ${notification.value}" in {
+        val request = FakeRequest().withBody(Json.toJson(notificationItem(notification)))
 
-      result.header.status shouldBe Status.INTERNAL_SERVER_ERROR
+        val controller = newController(handleCommand = _ => Future.successful(Left(CommandError("not good"))))
+        val result = controller.callback()(request).futureValue
+
+        result.header.status shouldBe Status.INTERNAL_SERVER_ERROR
+      }
+
+      s"return BadRequest if EnvelopeId not found for ${notification.value}" in {
+        val request = FakeRequest().withBody(Json.toJson(notificationItem(notification)))
+
+        val controller = newController(handleCommand = _ => Future.successful(Left(EnvelopeNotFoundError)))
+        val result = controller.callback()(request).futureValue
+
+        result.header.status shouldBe Status.BAD_REQUEST
+      }
+
+      s"return response with 503 for ${notification.value}" in {
+        val request = FakeRequest().withBody(Json.toJson(notificationItem(notification)))
+
+        val controller = newController(handleCommand = _ => failed)
+        val result = controller.callback()(request).futureValue
+
+        result.header.status shouldBe Status.SERVICE_UNAVAILABLE
+      }
     }
 
-    "return BadRequest if EnvelopeId not found" in {
-      val request = FakeRequest().withBody(Json.toJson(notificationItem(FileProcessed)))
-
-      val controller = newController(handleCommand = _ => Future.successful(Left(EnvelopeNotFoundError)))
-      val result = controller.callback()(request).futureValue
-
-      result.header.status shouldBe Status.BAD_REQUEST
-    }
-
-    "return 200 response if handling duplicate request" in {
+    "return 200 response if handling duplicate request for FileProcessed" in {
       val request = FakeRequest().withBody(Json.toJson(notificationItem(FileProcessed)))
 
       val controller = newController(handleCommand = _ => Future.successful(Left(EnvelopeArchivedError)))
@@ -93,26 +106,26 @@ class SDESCallbackControllerSpec
       result.header.status shouldBe Status.OK
     }
 
-    "return response with 503" in {
-      val request = FakeRequest().withBody(Json.toJson(notificationItem(FileProcessed)))
+    "return 200 response if handling duplicate request for FileReceived" in {
+      val request = FakeRequest().withBody(Json.toJson(notificationItem(FileReceived)))
 
-      val controller = newController(handleCommand = _ => failed)
+      val controller = newController(handleCommand = _ => Future.successful(Left(EnvelopeAlreadyRoutedError)))
       val result = controller.callback()(request).futureValue
 
-      result.header.status shouldBe Status.SERVICE_UNAVAILABLE
+      result.header.status shouldBe Status.OK
     }
   }
 
   private def notificationItem(notification: Notification) =
     NotificationItem(
-      notification = notification,
-      informationType = Some("S18"),
-      filename = "ourref.xyz.doc",
+      notification      = notification,
+      informationType   = Some("S18"),
+      filename          = "ourref.xyz.doc",
       checksumAlgorithm = MD5,
-      checksum = "83HQWQ93D909Q0QWIJQE39831312EUIUQIWOEU398931293DHDAHBAS",
-      correlationId = "d1800c47-29b0-440a-9e2e-9d7362795e10",
-      availableUntil = Some(Instant.now()),
-      failureReason = Some("We were unable to successfully do some action"),
-      dateTime = Instant.now()
+      checksum          = "83HQWQ93D909Q0QWIJQE39831312EUIUQIWOEU398931293DHDAHBAS",
+      correlationId     = "d1800c47-29b0-440a-9e2e-9d7362795e10",
+      availableUntil    = Some(Instant.now()),
+      failureReason     = Some("We were unable to successfully do some action"),
+      dateTime          = Instant.now()
     )
 }
