740edf1ed40d4395fcedc910bfaa80320a4a4789
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2023-02-08 14:12:11 +0000

    Address PR bot comments

--- app/uk/gov/hmrc/fileupload/GlobalErrorHandler.scala
+++ app/uk/gov/hmrc/fileupload/GlobalErrorHandler.scala
@@ -21,13 +21,13 @@ import play.api.http.HttpErrorHandler
 import play.api.mvc.{RequestHeader, Result}
 import uk.gov.hmrc.fileupload.controllers.ExceptionHandler
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.Future
 
 @Singleton
 class GlobalErrorHandler @Inject() extends HttpErrorHandler {
   override def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] =
-    Future(ExceptionHandler(statusCode, message))(ExecutionContext.global)
+    Future.successful(ExceptionHandler(statusCode, message))
 
   override def onServerError(request: RequestHeader, exception: Throwable): Future[Result] =
-    Future(ExceptionHandler(exception))(ExecutionContext.global)
+    Future.successful(ExceptionHandler(exception))
 }
